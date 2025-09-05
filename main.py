a78e9dfeee7825adac45921884e6ece71d8bab8e
vxoid <cursed.void.dl@gmail.com>
2025-09-05 23:40:42 +0300

    +threads

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ with open("config.json", "r", encoding="utf-8") as file:
 
 api_id = config["api_id"]
 api_hash = config["api_hash"]
-chat_ids = config["chat_ids"]
+chats = config["chats"]
 messages = config["messages"]
 wait_for = config["wait_for"]
 attachment_dir = config["attachment_dir"]
@@ -26,35 +26,35 @@ os.makedirs(SESSION_FOLDER, exist_ok=True)
 app = Client("session", api_id=api_id, api_hash=api_hash, workdir=SESSION_FOLDER, device_model="autoadv", client_platform=ClientPlatform.ANDROID, app_version="Android 11.14.1")
 async def main():
   async with app:
-    for chat_id in chat_ids:
-      await app.get_chat(int(chat_id))
-      async for m in app.get_chat_history(int(chat_id), limit=1):
+    for chat in chats:
+      await app.get_chat(int(chat["id"]))
+      async for m in app.get_chat_history(int(chat["id"]), limit=1):
         pass
     
     while True:
       max_concurrency = 5
       semaphore = asyncio.Semaphore(max_concurrency)
 
-      async def sem_task(msg, chat_id, attachment=None):
+      async def sem_task(msg, chat_id, thread_id=None, attachment=None):
         async with semaphore:
           try:
-            await app.send_photo(chat_id, photo=attachment, caption=msg, parse_mode=ParseMode.HTML)
+            await app.send_photo(chat_id, photo=attachment, caption=msg, parse_mode=ParseMode.HTML, message_thread_id=thread_id)
           except FloodWait as flood_wait:
             logger.warning(f"FloodWait for {flood_wait.value}...")
             await asyncio.sleep(flood_wait.value)
-            return sem_task(msg, chat_id, attachment=attachment)
+            return sem_task(msg, chat_id, thread_id=thread_id, attachment=attachment)
           except SlowmodeWait as flood_wait:
             logger.warning(f"SlowmodeWait for {flood_wait.value}...")
             await asyncio.sleep(flood_wait.value)
-            return sem_task(msg, chat_id, attachment=attachment)
+            return sem_task(msg, chat_id, thread_id=thread_id, attachment=attachment)
           except Exception as e:
             tb_str = traceback.format_exc()
             logger.warning(f"failed to send message: {e} / {tb_str}")
             raise e
 
       tasks = []
-      for chat_id in chat_ids:
-        tasks.append(asyncio.create_task(sem_task(random.choice(messages), chat_id, attachment=random_file(attachment_dir))))
+      for chat in chats:
+        tasks.append(asyncio.create_task(sem_task(random.choice(messages), chat["id"], thread_id=chat.get("thread_id"), attachment=random_file(attachment_dir))))
         await asyncio.sleep(0.2)
 
       results = await asyncio.gather(*tasks, return_exceptions=True)
@@ -64,7 +64,7 @@ async def main():
           continue
 
         suc += 1
-      logger.warning(f"sent to {suc}/{len(chat_ids)}, waiting {wait_for} secs...")
+      logger.warning(f"sent to {suc}/{len(chats)}, waiting {wait_for} secs...")
       await asyncio.sleep(wait_for)
 
 def list_image_files(directory, recursive=False, exts=None):
